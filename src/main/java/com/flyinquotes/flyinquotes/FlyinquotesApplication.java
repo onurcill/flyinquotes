0cc071239b8121b1331d39ae6ae88c7834a83681
Onur Cil <onurcil08@gmail.com>
2018-05-03 00:26:45 +0300

    Spring boot project structure

---
+++ src/main/java/com/flyinquotes/flyinquotes/FlyinquotesApplication.java
@@ -0,0 +1,12 @@
+package com.flyinquotes.flyinquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyinquotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyinquotesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/flyinquotes/flyinquotes/FlyinquotesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flyinquotes.flyinquotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlyinquotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
