18e53bbec1ae6c9582d1a2e1e4d5b3a8cd18b255
Onur Cil <onurcil08@gmail.com>
2018-05-03 21:29:48 +0300

    http://localhost:8080/svc/v1/tweets/%23bitcoin yapıstır

--- src/main/java/com/flyinquotes/flyinquotes/Controllers/PostController.java
+++ src/main/java/com/flyinquotes/flyinquotes/Controllers/PostController.java
@@ -1,13 +1,34 @@
 package com.flyinquotes.flyinquotes.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import java.util.List;
+
 
 @RestController
+@RequestMapping(PostController.TWITTER_BASE_URI)
 public class PostController {
 	
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
+	public static final String TWITTER_BASE_URI = "svc/v1/tweets";
+	
+	@Autowired
+	private Twitter twitter;
+			
+	@RequestMapping(value="{hashTag}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public List<Tweet> getTweets(@PathVariable final String hashTag) {
+		List<Tweet> t = twitter.searchOperations().search(hashTag).getTweets();
+		for (Tweet te : t) {
+			System.out.println(te.getText());
+			System.out.println("-------------------------------------------------------------");
+		}
+		
+		return twitter.searchOperations().search(hashTag).getTweets();
 	}
+
 }
--- src/main/java/com/flyinquotes/flyinquotes/FlyinquotesApplication.java
+++ src/main/java/com/flyinquotes/flyinquotes/FlyinquotesApplication.java
@@ -2,17 +2,9 @@ package com.flyinquotes.flyinquotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 
 @SpringBootApplication
-public class FlyinquotesApplication extends SpringBootServletInitializer {
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FlyinquotesApplication.class);
-	}
+public class FlyinquotesApplication{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FlyinquotesApplication.class, args);
